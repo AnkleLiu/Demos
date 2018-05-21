520084dbb703753cc0365738a4fd91d94118c4e4
Uncle Liu <18643094191@163.com>
2018-05-21 10:30:56 +0800

    改变样式

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -42,23 +42,20 @@ const Search = ({ value, onChange, children }) =>
 
 
 const Table = ({ list, pattern, onDismiss }) =>
-    <div>
+    <div className="table">
         {
             list.filter(isSearched(pattern)).map(item => (
-                <div key={item.objectID}>
-                    <span>
+                <div key={item.objectID} className="table-row">
+                    <span style={{ width: '40%' }}>
                         <a href={item.url}>{item.url}</a>
                     </span>
-                    -
-                    <span>{item.title}</span>
-                    -
-                    <span>{item.author}</span>
-                    -
-                    <span>{item.num_comments}</span>
-                    -
-                    <span>{item.points}</span>
-                    <span>
+                    <span style={{ width: '30%' }}>{item.title}</span>
+                    <span style={{ width: '30%' }}>{item.author}</span>
+                    <span style={{ width: '10%' }}>{item.num_comments}</span>
+                    <span style={{ width: '10%' }}>{item.points}</span>
+                    <span style={{ width: '10%' }}>
                         <Button
+                            className="button-inline"
                             onClick={() => onDismiss(item.objectID)}>
                             Dismiss
                         </Button>
@@ -108,13 +105,15 @@ class App extends Component {
   render() {
       const { searchTerm, list } = this.state
         return (
-            <div className="App">
-                <Search
-                    value={searchTerm}
-                    onChange={this.onSearchChange}
-                >
-                    Search
-                </Search>
+            <div className="page">
+                <div className="interactions">
+                    <Search
+                        value={searchTerm}
+                        onChange={this.onSearchChange}
+                    >
+                        Search
+                    </Search>
+                </div>
                 <Table
                     list={list}
                     pattern={searchTerm}
