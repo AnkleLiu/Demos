7417d5840f9f6c15e4a2ccb0aeba4f67341a4116
Uncle Liu <18643094191@163.com>
2018-05-22 18:06:15 +0800

    用真实 API 搜索

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-    {
-        title: 'React',
-        url: 'https://google.com',
-        author: 'Someone',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    },
-    {
-        title: 'Redux',
-        url: 'z.cn',
-        author: 'who',
-        num_comments: 4,
-        points: 6,
-        objectID: 1,
-    }
-]
+const DEFAULT_QUERY = 'redux'
+const PATH_BASE = 'https://hn.algolia.com/api/v1'
+const PATH_SEARCH = '/search'
+const PARAM_SEARCH = 'query='
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`
+
+// const list = [
+//     {
+//         title: 'React',
+//         url: 'https://google.com',
+//         author: 'Someone',
+//         num_comments: 3,
+//         points: 4,
+//         objectID: 0,
+//     },
+//     {
+//         title: 'Redux',
+//         url: 'z.cn',
+//         author: 'who',
+//         num_comments: 4,
+//         points: 6,
+//         objectID: 1,
+//     }
+// ]
 
 function isSearched(searchTerm) {
     return function(item) {
@@ -85,11 +92,29 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            list: list,
-            searchTerm: '',
+            result: null,
+            searchTerm: DEFAULT_QUERY,
         }
+        this.setSearchTopStories = this.setSearchTopStories.bind(this)
+        this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this)
         this.onDismiss = this.onDismiss.bind(this)
         this.onSearchChange = this.onSearchChange.bind(this)
+    }
+
+    setSearchTopStories(result) {
+        this.setState({ result })
+    }
+
+    fetchSearchTopStories(searchTerm) {
+        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`)
+            .then(response => response.json())
+            .then(result => this.setSearchTopStories(result))
+            .catch(e => e)
+    }
+
+    componentDidMount() {
+        const { searchTerm } = this.state
+        this.fetchSearchTopStories(searchTerm)
     }
 
     onDismiss(id) {
@@ -103,7 +128,12 @@ class App extends Component {
     }
 
   render() {
-      const { searchTerm, list } = this.state
+      const { searchTerm, result } = this.state
+
+      if (!result) {
+          return null
+      }
+
         return (
             <div className="page">
                 <div className="interactions">
@@ -115,7 +145,7 @@ class App extends Component {
                     </Search>
                 </div>
                 <Table
-                    list={list}
+                    list={result.hits}
                     pattern={searchTerm}
                     onDismiss={this.onDismiss}
                 />
