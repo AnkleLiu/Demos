10b550e209fabd6a03487e22adea07d036bda3b8
Uncle Liu <18643094191@163.com>
2018-05-23 13:04:20 +0800

    增加缓存

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -69,7 +69,9 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            result: null,
+            // result: null,
+            results: null,
+            searchKey: '',
             searchTerm: DEFAULT_QUERY,
         }
         this.onSearchSubmit = this.onSearchSubmit.bind(this)
@@ -77,17 +79,23 @@ class App extends Component {
         this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this)
         this.onDismiss = this.onDismiss.bind(this)
         this.onSearchChange = this.onSearchChange.bind(this)
+        this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this)
     }
 
     setSearchTopStories(result) {
         const { hits, page } = result
-        const oldHits = page !== 0 ? this.state.result.hits : []
+        const { searchKey, results } = this.state
+        const oldHits = results && results[searchKey]
+                        ? results[searchKey].hits : []
         const updatedHits = [
             ...oldHits,
             ...hits,
         ]
         this.setState({
-            result: { hits: updatedHits, page}
+            results: {
+                ...results,
+                [searchKey]: { hits: updatedHits, page }
+            }
         })
     }
 
@@ -100,22 +108,35 @@ class App extends Component {
 
     onSearchSubmit(event) {
         const { searchTerm } = this.state
-        this.fetchSearchTopStories(searchTerm)
+        this.setState({ searchKey: searchTerm })
+        if (this.needsToSearchTopStories(searchTerm)) {
+            this.fetchSearchTopStories(searchTerm)
+        }
         event.preventDefault()
     }
 
     componentDidMount() {
         const { searchTerm } = this.state
+        this.setState({ searchKey: searchTerm })
         this.fetchSearchTopStories(searchTerm)
     }
 
     onDismiss(id) {
+        const { searchKey, results } = this.state
+        const { hits, page } = results[searchKey]
         // filter 返回新列表
         const isNotId = item => item.objectID !== id
-        const updatedHits = this.state.result.hits.filter(isNotId)
+        const updatedHits = hits.filter(isNotId)
         this.setState({
-            result: { ...this.state.result, hits: updatedHits }
+            results: {
+                ...results,
+                [searchKey]: { hits: updatedHits, page }
+            }
         })
+    }
+
+    needsToSearchTopStories(searchTerm) {
+        return !this.state.results[searchTerm]
     }
 
     onSearchChange(event) {
@@ -123,11 +144,19 @@ class App extends Component {
     }
 
   render() {
-      const { searchTerm, result } = this.state
-      const page = (result && result.page) || 0
-      if (!result) {
-          return null
-      }
+      const { searchTerm, results, searchKey } = this.state
+      const page = (
+            results &&
+            results[searchKey] &&
+            results[searchKey].page) || 0
+      // if (!result) {
+      //     return null
+      // }
+      const list = (
+          results &&
+          results[searchKey] &&
+          results[searchKey].hits
+      ) || []
       return (
             <div className="page">
                 <div className="interactions">
@@ -140,14 +169,14 @@ class App extends Component {
                     </Search>
                 </div>
                 {
-                    result &&
+                    results &&
                     <Table
-                        list={result.hits}
+                        list={list}
                         onDismiss={this.onDismiss}
                     />
                 }
                 <div className="interactions">
-                    <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+                    <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
                         More
                     </Button>
                 </div>
